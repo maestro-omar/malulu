8b717489424aa118250cdd7cc011690c1194592a
omarnts <omar@nitsnets.com>
2025-06-06 17:38:30 -0300

    Playing with tailwind css color gradient for nav header

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -20,7 +20,7 @@ onMounted(() => {
 <template>
     <div>
         <div class="min-h-screen bg-gray-100">
-            <nav class="bg-white border-b border-gray-100">
+            <nav class="bg-gradient-to-b from-red-900 to-blue-900 border-b border-gray-100">
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
@@ -29,7 +29,7 @@ onMounted(() => {
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('dashboard')">
                                     <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
+                                        class="block h-9 w-auto fill-current text-white"
                                     />
                                 </Link>
                             </div>
@@ -37,7 +37,7 @@ onMounted(() => {
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <template v-for="item in $page.props.menu.items" :key="item.route">
-                                    <NavLink :href="route(item.route)" :active="route().current(item.route)">
+                                    <NavLink :href="route(item.route)" :active="route().current(item.route)" class="text-white hover:text-gray-200">
                                         {{ item.name }}
                                     </NavLink>
                                 </template>
@@ -52,7 +52,7 @@ onMounted(() => {
                                         <span class="inline-flex rounded-md">
                                             <button
                                                 type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-white hover:text-gray-200 focus:outline-none transition ease-in-out duration-150"
                                             >
                                                 {{ $page.props.auth.user.name }}
 
@@ -86,7 +86,7 @@ onMounted(() => {
                         <div class="-me-2 flex items-center sm:hidden">
                             <button
                                 @click="showingNavigationDropdown = !showingNavigationDropdown"
-                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                                class="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-gray-200 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                             >
                                 <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                                     <path
@@ -122,7 +122,7 @@ onMounted(() => {
                 >
                     <div class="pt-2 pb-3 space-y-1">
                         <template v-for="item in $page.props.menu.items" :key="item.route">
-                            <ResponsiveNavLink :href="route(item.route)" :active="route().current(item.route)">
+                            <ResponsiveNavLink :href="route(item.route)" :active="route().current(item.route)" class="text-white hover:text-gray-200">
                                 {{ item.name }}
                             </ResponsiveNavLink>
                         </template>
@@ -131,15 +131,15 @@ onMounted(() => {
                     <!-- Responsive Settings Options -->
                     <div class="pt-4 pb-1 border-t border-gray-200">
                         <div class="px-4">
-                            <div class="font-medium text-base text-gray-800">
+                            <div class="font-medium text-base text-white">
                                 {{ $page.props.auth.user.name }}
                             </div>
-                            <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
+                            <div class="font-medium text-sm text-gray-200">{{ $page.props.auth.user.email }}</div>
                         </div>
 
                         <div class="mt-3 space-y-1">
-                            <ResponsiveNavLink :href="route('profile.edit')"> Perfil </ResponsiveNavLink>
-                            <ResponsiveNavLink :href="route('logout.get')" as="button">
+                            <ResponsiveNavLink :href="route('profile.edit')" class="text-white hover:text-gray-200"> Perfil </ResponsiveNavLink>
+                            <ResponsiveNavLink :href="route('logout.get')" as="button" class="text-white hover:text-gray-200">
                                 Salir
                             </ResponsiveNavLink>
                         </div>
