3e60004bac7595b20d8ab5a0bf242672e2fdcb86
omarnts <omar@nitsnets.com>
2025-06-30 01:20:36 -0300

    Important routing files order: public to last postition for "lonely" school slug

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-require __DIR__ . '/public.php';
-require __DIR__ . '/auth.php';
 require __DIR__ . '/json-options.php';
+require __DIR__ . '/auth.php';
 require __DIR__ . '/system.php';
 require __DIR__ . '/school.php';
+require __DIR__ . '/public.php';//important to be the last one because path {school slug} is for searching any other first slug to match a school 
